9763a296638247c6b96d3d7c35a2918410f2f003
yang <997052986@qq.com>
2023-11-09 21:17:00 +0800

    remove task

--- src/main/java/com/apps/sky/starter/Main.java
+++ src/main/java/com/apps/sky/starter/Main.java
@@ -1,8 +1,6 @@
 package com.apps.sky.starter;
 
-import com.origin.starter.app.spi.OriginTask;
 import com.origin.starter.web.OriginWebApplication;
-import com.origin.starter.web.spi.OriginRouter;
 import io.vertx.core.AbstractVerticle;
 
 import java.util.ServiceLoader;
@@ -10,8 +8,6 @@ import java.util.ServiceLoader;
 public class Main extends AbstractVerticle {
 
   public static void main(String[] args) {
-    ServiceLoader<OriginRouter> loader = ServiceLoader.load(OriginRouter.class);
-    ServiceLoader<OriginTask> task = ServiceLoader.load(OriginTask.class);
     OriginWebApplication.runAsSingle(Main.class);
   }
 }
