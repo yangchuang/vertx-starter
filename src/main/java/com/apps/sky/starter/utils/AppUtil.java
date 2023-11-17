bf1b275b63fe090e2f8d1e98ac018c0fa5044b4d
yang <997052986@qq.com>
2023-11-18 01:45:04 +0800

    添加保存图片的耗时日志

--- src/main/java/com/apps/sky/starter/utils/AppUtil.java
+++ src/main/java/com/apps/sky/starter/utils/AppUtil.java
@@ -1,6 +1,9 @@
 package com.apps.sky.starter.utils;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.TimeInterval;
 import cn.hutool.core.io.FileUtil;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -9,6 +12,7 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
+@Slf4j
 public class AppUtil {
   /**
    * @param imageUrl e.g. "https://skytools.cn/images/poetry/3.jpeg"
@@ -16,6 +20,7 @@ public class AppUtil {
    * @throws IOException
    */
   public static void saveImage(String imageUrl, String destinationPath) throws IOException {
+    TimeInterval timer = DateUtil.timer();
     // 创建URL对象
     URL url = new URL(imageUrl);
     // 创建目标文件
@@ -28,5 +33,6 @@ public class AppUtil {
       // 使用NIO的Channel将输入流写入文件
       out.getChannel().transferFrom(channel, 0, Long.MAX_VALUE);
     }
+    log.warn("从URL获取图片保存到文件系统中耗时：{} s", timer.interval()/1000);
   }
 }
